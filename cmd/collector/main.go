872ebb5dfda44d0bdd6156da1fde021ed4a76ad0
Chris Suszyński <ksuszyns@redhat.com>
2025-06-11 20:12:04 +0200

    Fetchin only interesting data

--- cmd/collector/main.go
+++ cmd/collector/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 
 	"gopkg.in/mcuadros/go-syslog.v2"
 )
@@ -36,9 +38,27 @@ func main() {
 
 	go func(channel syslog.LogPartsChannel) {
 		for logParts := range channel {
-			fmt.Printf("%#v\n", logParts)
+			data := ClusterLoggingMsg{}
+			if err := json.Unmarshal([]byte(fmt.Sprint(logParts["message"])), &data); err != nil {
+				panic(err)
+			}
+			bytes, _ := json.MarshalIndent(data, "", "  ")
+			fmt.Printf("%s\n", string(bytes))
 		}
 	}(channel)
 
 	server.Wait()
+}
+
+type Kubernetes struct {
+	ContainerName  string `json:"container_name"`
+	ContainerImage string `json:"container_image"`
+	NamespaceName  string `json:"namespace_name"`
+	PodName        string `json:"pod_name"`
+}
+
+type ClusterLoggingMsg struct {
+	Timestamp  time.Time `json:"timestamp"`
+	Message    string    `json:"message"`
+	Kubernetes `json:"kubernetes"`
 }
